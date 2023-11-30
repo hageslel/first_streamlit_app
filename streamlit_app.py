bab28bd43cf78352bc15bcaad52f45c323147d81
Lee Hages <hageslel@gmail.com>
2023-11-30 11:38:27 -0600

    Update streamlit_app.py
    Putting json response into a df

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# Put json respons into a df using pandas
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Display the data as a df
+streamlit.dataframe(fruityvice_normalized)
