162b4a20a3b6636a5f7a6cc8ed32ec500f6d9ed6
Lee Hages <hageslel@gmail.com>
2023-12-01 14:00:54 -0600

    Update streamlit_app.py
    Updated code to add snowflake commands into a function.

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,14 +39,20 @@ try:
       streamlit.dataframe(back_from_function)
 except URLError as e:
       streamlit.error()
-streamlit.stop()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+# Snowflake related functions
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+        return my_cur.fetchall()
+
+
+# Add a button to load fruit
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])  
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
 
 # Adding a new section to add fruits to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'Jackfruit')
