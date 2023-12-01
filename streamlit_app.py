6b0b2c610cfb379e2a54645f8bf0f171a504e5aa
Lee Hages <hageslel@gmail.com>
2023-12-01 13:15:45 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows).append(add_my_fruit)
+streamlit.dataframe(my_data_rows)
 
 # Adding a new section to add fruits to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'Jackfruit')
